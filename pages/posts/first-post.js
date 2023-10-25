afba9ed48390c6eea8f86268905f2a29de8cb35d
JamesAskelson <jamesaskelson630@gmail.com>
2023-10-25 18:28:23 -0500

    nextjs-head-editting

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,29 @@
 import Link from 'next/link';
+import Image from 'next/image'
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
+                />
+            </Head>
+
             <h1>First Post</h1>
+
+            <Image
+            src="/images/profile.jpg"
+            height={200}
+            width={200}
+            alt='barkley'
+            ></Image>
+
             <Link href="/">Back to home</Link>
         </>
 
